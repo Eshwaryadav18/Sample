ecff91e870ea82b14ffc0c76b7fd4eab0d8d9699
2431688 <2431688@LTIN647984.cts.com>
2025-07-24 17:07:10 +0530

    FIRST COMMIT

---
+++ Project/src/com/adm_packages/App1.java
@@ -0,0 +1,10 @@
+package com.adm_packages;
+
+public class App1 {
+
+	public static void main(String[] args) {
+		System.out.println("App1");
+
+	}
+
+}
---
+++ Project/src/com/adm_packages/App2.java
@@ -0,0 +1,10 @@
+package com.adm_packages;
+
+public class App2 {
+
+	public static void main(String[] args) {
+		System.out.println("App2");
+
+	}
+
+}
